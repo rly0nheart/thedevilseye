ea9cf8664240fdccc660d5b56bedd2d0ec008f64
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-09-01 15:43:33 +0200

    Create banner.py

---
+++ assets/banner.py
@@ -0,0 +1,12 @@
+from assets.colors import red,white,reset
+banner = f""""{red}
+▄▄▄▄▄ ▄ .▄▄▄▄ .    ·▄▄▄▄  ▄▄▄ . ▌ ▐·▪  ▄▄▌  .▄▄ ·     ▄▄▄ . ▄· ▄▌▄▄▄ .
+•██  ██▪▐█▀▄.▀·    ██▪ ██ ▀▄.▀·▪█·█▌██ ██•  ▐█ ▀.     ▀▄.▀·▐█▪██▌▀▄.▀·
+ ▐█.▪██▀▐█▐▀▀▪▄    ▐█· ▐█▌▐▀▀▪▄▐█▐█•▐█·██▪  ▄▀▀▀█▄    ▐▀▀▪▄▐█▌▐█▪▐▀▀▪▄
+ ▐█▌·██▌▐▀▐█▄▄▌    ██. ██ ▐█▄▄▌ ███ ▐█▌▐█▌▐▌▐█▄▪▐█    ▐█▄▄▌ ▐█▀·.▐█▄▄▌
+ ▀▀▀ ▀▀▀ · ▀▀▀     ▀▀▀▀▀•  ▀▀▀ . ▀  ▀▀▀.▀▀▀  ▀▀▀▀      ▀▀▀   ▀ •  ▀▀▀ 
+{red}Disclaimer{white}:
+This tool was developed for educational purposes and should not be used in environments
+without legal authorization.
+Therefore, the author shall not be responsible for the damages that might be done with it.{reset}
+"""
