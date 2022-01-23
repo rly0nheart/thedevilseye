9571b298dbd454d46682ab9240fe6700a6855138
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-01-23 22:34:46 +0200

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -11,7 +11,5 @@ if not colors:
 else:                                                 
     white = "\033[97m"
     red = "\033[91m"                                  
-    yellow = "\033[93m"
     reset = "\033[0m"
-    brightred = "\033[31;1m"
     green = "\033[92m"
