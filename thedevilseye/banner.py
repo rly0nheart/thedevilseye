03079da8685087cce608790b9be101ae9ae637e2
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-12-02 21:42:33 +0200

    Create banner.py

---
+++ thedevilseye/banner.py
@@ -0,0 +1,9 @@
+banner = """
+  o  o           o             o                 
+  |  |           |           o |                 
+ -o- O--o o-o  o-O o-o o   o   | o-o o-o o  o o-o
+  |  |  | |/  |  | |/   \ /  | |  \  |/  |  | |/ 
+  o  o  o o-o  o-o o-o   o   | o o-o o-o o--O o-o
+                                            |    
+                    [red]2022.1.5.0#hellraiser[/]o--o
+"""
