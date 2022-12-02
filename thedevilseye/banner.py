1a0da44806236514515d621418c3f742102e74fd
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-12-02 22:18:22 +0200

    Delete banner.py

--- thedevilseye/banner.py
+++
@@ -1,9 +0,0 @@
-banner = """
-  o  o           o             o                 
-  |  |           |           o |                 
- -o- O--o o-o  o-O o-o o   o   | o-o o-o o  o o-o
-  |  |  | |/  |  | |/   \ /  | |  \  |/  |  | |/ 
-  o  o  o o-o  o-o o-o   o   | o o-o o-o o--O o-o
-                                            |    
-                    [red]2022.1.5.0-hellfire#6[/]o--o
-"""
