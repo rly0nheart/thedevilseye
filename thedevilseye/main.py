0b80ed2900d67c7538795effe4a9dada452fcd14
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-12-02 22:22:48 +0200

    Update main.py

--- thedevilseye/main.py
+++ thedevilseye/main.py
@@ -3,6 +3,13 @@
 from thedevilseye.thedevilseye *
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(description="Darkweb .onion link(s) extracting tool  — by Richard Mwewa | https://about.me/rly0nheart",epilog="thedevilseye scrapes darkweb search engines and gets information (.onion links, descriptions) requiring a Tor network. Developed by Richard Mwewa | https://about.me/rly0nheart")
+    parser.add_argument('query', help='search quey')
+    parser.add_argument("-d","--dump", metavar="path/to/file", help=argparse.SUPPRESS)
+    return parser
+
+
 def main():
     devils_eye = TheDevilsEye() 
     # Parse command-line arguments
@@ -10,7 +17,8 @@ def main():
     args = parser.parse_args()
 
     start_time = datetime.now()
-    xprint(banner)
+    xprint("[[red]THEDEVILSEYE[/]] v2022.1.5.0-[red]hellfire#6[/]")
+)
     xprint(f"\n[[green]STARTED[/]] initialized: <time_dt={start_time}, engine=[green]{args.engine}[/], query=[green]{args.query}[/]>")
     try:
         devils_eye.search_ahmia_fi(query=args.query)
