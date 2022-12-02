23c02e4c7afc08f91f6557d2ec9855525537404d
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-12-02 22:14:22 +0200

    Update main.py

--- thedevilseye/main.py
+++ thedevilseye/main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
-from rich import print as xprint
-from thedevilseye.banner import banner
-from thedevilseye.thedevilseye import TheDevilsEye
-from thedevilseye.thedevilseye import create_parser
+from thedevilseye.thedevilseye *
 
 
 def main():
@@ -16,14 +12,9 @@ def main():
     start_time = datetime.now()
     xprint(banner)
     xprint(f"\n[[green]STARTED[/]] initialized: <time_dt={start_time}, engine=[green]{args.engine}[/], query=[green]{args.query}[/]>")
-
-    if args.query == None:
-        exit('[[red]NEGATIVE[/]] Search query not specified (-q/--query).')
-    if args.engine == 'ahmia.fi':
+    try:
         devils_eye.search_ahmia_fi(query=args.query)
-    elif args.engine == 'tor.link':
-        devils_eye.search_tor_link(query=args.query)
-    else:
-      exit('thedevilseye: use -h/--help to show usage.')
+    except Exception as e:
+        xprint(f"[[red]ERROR[/]] An error occurred: [red]{e}[/]")
 
     xprint(f"[[green]COMPLETE[/]] Complete: <time_sec={datetime.now() - start_time}>")
