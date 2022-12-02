5d8e80b95e33dcdf0604e96b493f3f46a5672b61
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-12-02 21:53:22 +0200

    Update banner.py

--- thedevilseye/banner.py
+++ thedevilseye/banner.py
@@ -5,5 +5,5 @@ banner = """
   |  |  | |/  |  | |/   \ /  | |  \  |/  |  | |/ 
   o  o  o o-o  o-o o-o   o   | o o-o o-o o--O o-o
                                             |    
-                    [red]2022.1.5.0#hellraiser[/]o--o
+                    [red]2022.1.5.0-hellfire#6[/]o--o
 """
