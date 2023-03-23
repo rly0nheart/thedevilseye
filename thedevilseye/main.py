8abf299c5771e3887f366b66c13b55cec2c563bf
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-03-24 00:21:54 +0200

    Update main.py

--- thedevilseye/main.py
+++ thedevilseye/main.py
@@ -5,7 +5,7 @@ from thedevilseye.thedevilseye import *
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description="Darkweb .onion link(s) extracting tool  — by Richard Mwewa | https://about.me/rly0nheart",epilog="thedevilseye scrapes darkweb search engines and gets information (.onion links, descriptions) requiring a Tor network. Developed by Richard Mwewa | https://about.me/rly0nheart")
+    parser = argparse.ArgumentParser(description="Get tor hidden services and descriptions that match with the users query  — by Richard Mwewa | https://about.me/rly0nheart",epilog="thedevilseye is an osint tool that uses Ahmia.fi to get Tor hidden services and descriptions that match with the users query.")
     parser.add_argument('query', help='search quey')
     parser.add_argument("-d","--dump", metavar="path/to/file", help=argparse.SUPPRESS)
     return parser
@@ -17,12 +17,13 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
     start_time = datetime.now()
+    
     os.system("cls" if os.name == "nt" else "clear")
-    xprint("[[red]THEDEVILSEYE[/]] 2022.1.5.1-[red]hellfire#6[/]")
-    xprint(f"\n[[green]STARTED[/]] initialized: <time_dt={start_time}, query=[green]{args.query}[/]>")
+    xprint(f"{RED}THEDEVILSEYE{RESET} 1.5.2-{RED}hellfire#7{RESET}")
+    xprint(f"\n[{GREEN}*{RESET}] initialized <time_dt={start_time}, query='{args.query}'> ...")
     try:
         devils_eye.search_ahmia_fi(query=args.query)
     except Exception as e:
-        xprint(f"[[red]ERROR[/]] An error occurred: [red]{e}[/]")
+        xprint(f"[{RED}x{RESET}] An error occurred: {RED}{e}{RESET}")
 
-    xprint(f"[[green]COMPLETE[/]] Complete: <time_sec={datetime.now() - start_time}>")
+    xprint(f"[{GREEN}-{RESET}] Complete <time_sec={datetime.now() - start_time}>")
