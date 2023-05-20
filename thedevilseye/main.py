0f36ee0591920bbd5fce730f46fdb2279a0ec2ad
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-05-20 02:16:40 +0200

    Update main.py - 1.6.0

--- thedevilseye/main.py
+++ thedevilseye/main.py
@@ -2,16 +2,15 @@ import os
 import time
 from thedevilseye.thedevilseye import *
 
-parser = create_parser()
-arguments = parser.parse_args()
-
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
 
 def thedevilseye():
-    xprint(f"{COLOURS['RED']}THEDEVILSEYE{COLOURS['RESET']} 1.6.0-{COLOURS['RED']}hellfire#7{COLOURS['RESET']}")
+    if arguments.debug:
+        
+    xprint(f"{COLOURS['RED']}THEDEVILSEYE{COLOURS['RESET']} {current_version}-{COLOURS['RED']}hellfire#7{COLOURS['RESET']}")
     xprint(f"{COLOURS['GREEN']}[*]{COLOURS['RESET']} initialized <time_dt={time.asctime()}, query='{arguments.query}'> ...")
     try:
         get_hidden_services(query=arguments.query, result_count=arguments.count)
